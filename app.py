7ed7fff8dad955bfb619d4275e3d558d2bffc1c8
Amermm <mahmoudaamer87@gmail.com>
2025-01-25 17:04:21 +0300

    apy 8080

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ logging.basicConfig(level=logging.DEBUG)
 app = create_app()
 
 if __name__ == '__main__':
-    # Use port 8000 instead of the default 5000
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    # Use port 8080 instead of the default 5000
+    app.run(host='0.0.0.0', port=8080, debug=True)
+
     print("Running app.py from:", __file__)
