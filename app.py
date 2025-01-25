bffdc573cc5b4085e5ef9994c5d17597a446883b
Amermm <mahmoudaamer87@gmail.com>
2025-01-25 12:16:28 +0300

    app v1.3

--- app.py
+++ app.py
@@ -7,3 +7,4 @@ app = create_app()
 if __name__ == '__main__':
     # Use port 8000 instead of the default 5000
     app.run(host='0.0.0.0', port=8000, debug=True)
+    print("Running app.py from:", __file__)
