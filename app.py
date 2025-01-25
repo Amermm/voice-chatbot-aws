df787fd83e35a5c1a117677d3a5780eb8b4aeff9
Amermm <mahmoudaamer87@gmail.com>
2025-01-25 11:05:04 +0300

    app v1.2

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from app import create_app
+from app import create_app 
+import logging
+logging.basicConfig(level=logging.DEBUG)
 
 app = create_app()
 
 if __name__ == '__main__':
     # Use port 8000 instead of the default 5000
-    app.run(host='0.0.0.0', port=8000, debug=False)
+    app.run(host='0.0.0.0', port=8000, debug=True)
